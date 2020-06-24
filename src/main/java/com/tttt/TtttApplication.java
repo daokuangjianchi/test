282034897f892770b9afa269165a9650641eb6e2
jiayou <1192884177@qq.com>
2020-06-24 17:35:02 +0800

    1

---
+++ src/main/java/com/tttt/TtttApplication.java
@@ -0,0 +1,13 @@
+package com.tttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tttt/TtttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
